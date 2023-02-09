b4ecc5ec2484390256bb209a2dac94796e5c1586
unknown <chanchal kolekar>
2023-02-10 03:31:26 +0530

    I have done polymorphim

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
+    public static void main(String[] args){
+      Product p = new Product();
+      p.product(5,10);
+      p.product(1,2,3);
+      p.product(1.2,1.5);
+    }
+
 }
